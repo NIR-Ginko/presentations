50860ef64ca0fc7ed7f092a2b669c9d3e571435d
Igor Chudov <nir@altlinux.org>
2020-01-28 19:26:23 +0400

    Update R script to draw join request diagram

--- 2020-02-07 ALT Join and community/install_packages.r
+++ 2020-02-07 ALT Join and community/install_packages.r
@@ -6,7 +6,9 @@ main <- function() {
 	install.packages("docstring", repos = "http://cran.rstudio.com/")
 	install.packages("plyr", repos = "http://cran.rstudio.com/")
 	install.packages("ggplot2", repos = "http://cran.rstudio.com/")
-	#install.packages("rayshader", repos = "http://cran.rstudio.com/")
+	install.packages("RcppArmadillo", repos = "http://cran.rstudio.com/")
+	install.packages("magick", repos = "http://cran.rstudio.com/")
+	install.packages("rayshader", repos = "http://cran.rstudio.com/")
 	#devtools::install_github("tylermorganwall/rayshader")
 }
 
--- 2020-02-07 ALT Join and community/makestat.r
+++ 2020-02-07 ALT Join and community/makestat.r
@@ -3,11 +3,12 @@
 library(docstring)
 library(plyr)
 library(ggplot2)
+library(rgl)
+library(rayshader)
 
-print_join_requests <- function(bugzilla_tsd) {
-	#str(bugzilla_tsd)
-  bugzilla_tsd$opn <- as.Date(bugzilla_tsd$Opened, format = "%Y-%m-%d")
-  tab <- table(cut(bugzilla_tsd$opn, "month"))
+filter_data <- function(csv_data) {
+  csv_data$opn <- as.Date(csv_data$Opened, format = "%Y-%m-%d")
+  tab <- table(cut(csv_data$opn, "month"))
   #str(tab)
   #str(bugzilla_tsd)
   #filtered_tsd <- count(bugzilla_tsd, vars = "opn")
@@ -16,12 +17,33 @@ print_join_requests <- function(bugzilla_tsd) {
   group_tsd <- data.frame(Date = format(as.Date(names(tab)), "%Y-%m-%d"), Freq = as.vector(tab))
   group_tsd$op <- as.Date(group_tsd$Date, format = "%Y-%m-%d")
   str(group_tsd)
-  
-  tsd_ggplot <- ggplot2::ggplot(data = group_tsd) +
-  	ggplot2::geom_line(ggplot2::aes(x = op, y = Freq)) +
-  	ggplot2::labs(x = "Date", y = "Join requests", title = "Join requests") +
-  	ggplot2::theme_minimal()
+  return(group_tsd)
+}
+
+render_ggplot <- function(tsd) {
+  tsd_ggplot <- ggplot2::ggplot(data = tsd) +
+    ggplot2::labs(x = "Date", y = "Join requests", title = "Join requests") +
+    #ggplot2::geom_area(ggplot2::aes(x = op, y = Freq, fill="blue")) +
+    ggplot2::geom_point(ggplot2::aes(x = op, y = Freq)) +
+    ggplot2::scale_color_continuous()
+    #ggplot2::theme_classic() +
+    #ggplot2::theme(legend.position = "none")
+  ggplot2::ggsave("ggplot.png")
   print(tsd_ggplot)
+  return(tsd_ggplot)
+}
+
+ggplot_rayshader <- function(ggdata) {
+  rayshader::plot_gg(ggdata,multicore=TRUE,width=3,height=3,scale=500)
+  #filename_map = tempfile()
+  #write_png(filename_map, ggp)
+  #render_snapshot()
+}
+
+print_join_requests <- function(bugzilla_tsd) {
+  group_tsd <- filter_data(bugzilla_tsd)
+  ggtsd <- render_ggplot(group_tsd)
+  ggplot_rayshader(ggtsd)
 }
 
 main <- function() {
